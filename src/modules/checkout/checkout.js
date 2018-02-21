01dab75f145380c335b904a58eb27431e4516ccc
Chew Zhen Yong <zhenyong0802@gmail.com>
2018-02-21 17:39:32 +0800

    initial test

---
+++ src/modules/checkout/checkout.js
@@ -0,0 +1,3 @@
+export const CLASSIC_AD = 'CLASSIC';
+export const STANDOUT_AD = 'STANDOUT';
+export const PREMIUM_AD = 'PREMIUM';
---
+++ src/modules/checkout/checkout.test.js
@@ -0,0 +1,7 @@
+import {CLASSIC_AD, STANDOUT_AD, PREMIUM_AD} from './checkout';
+
+describe('checkout', () => {
+  it('should return true', () => {
+    expect(true).toEqual(true);
+  })
+})
