0ff3ea2af36cc3fa5e79e474a6b59da1e94d81e9
Chew Zhen Yong <zhenyong0802@gmail.com>
2018-02-22 15:23:56 +0800

    passed all basic test

--- src/modules/checkout/checkout.js
+++ src/modules/checkout/checkout.js
@@ -1,9 +1,48 @@
-import {defaultPrices} from '../ads/ads';
+import {adPrices, CLASSIC_AD, STANDOUT_AD, PREMIUM_AD} from '../ads/ads';
 export const DEFAULT = 'DEFAULT';
+export const UNILIVER = 'UNILIVER';
+export const APPLE = 'APPLE';
+export const NIKE = 'NIKE';
 
 export default (customer) => {
-  console.log(defaultPrices);
-  return (items) => {
-    return items.reduce((total, item) => parseFloat(total) + defaultPrices[item], [0])
+  switch(customer) {
+    case UNILIVER:
+      return (items) => {
+        return getTotalPrice(adPrices, items) - getMoreForLessDealDiscount(items, CLASSIC_AD, 3, 2);
+      }
+    case APPLE:
+      return (items) => {
+        const applePrices = getDiscountPrice(adPrices, STANDOUT_AD, 299.99)
+        return getTotalPrice(applePrices, items);
+      }
+    case NIKE:
+      return (items) => {
+        return getTotalPrice(getDiscountPriceWhenBuyMore(items, adPrices, PREMIUM_AD, 379.99, 4), items);
+      }
+    default:
+      return (items) => {
+        return getTotalPrice(adPrices, items);
+      }
   }
+}
+
+const getTotalPrice = (adPrices, items) => {
+  return items.reduce((total, item) => parseFloat(total) + adPrices[item], [0]);
+}
+
+const getMoreForLessDealDiscount = (items, adType, purchasedAds, billingAds) => {
+  const filteredItem = items.filter((item) => item === adType);
+  const freeAds = Math.floor(filteredItem.length / purchasedAds) * (purchasedAds - billingAds);
+  return freeAds * adPrices[adType];
+}
+
+const getDiscountPrice = (adPrices, adType, price) => {
+  return { ...adPrices, [adType]: price};
+}
+
+const getDiscountPriceWhenBuyMore = (items, adPrices, adType, price, minAd) => {
+  if(items.filter(item => item === adType ).length >= minAd) {
+    return { ...adPrices, [adType]: price};
+  }
+  return adPrices;
 }
--- src/modules/checkout/checkout.test.js
+++ src/modules/checkout/checkout.test.js
@@ -1,9 +1,24 @@
-import {CLASSIC_AD, STANDOUT_AD, PREMIUM_AD} from '../ads/ads';
-import checkout, {DEFAULT} from './checkout';
+import { CLASSIC_AD, STANDOUT_AD, PREMIUM_AD } from '../ads/ads';
+import checkout, {DEFAULT, UNILIVER, APPLE, NIKE} from './checkout';
 
 describe('checkout', () => {
   it('should correct for Default', () => {
     const total = checkout(DEFAULT)([CLASSIC_AD,STANDOUT_AD,PREMIUM_AD]);
-    expect(total).toEqual(987.97);
-  })
+    expect(total).toBeCloseTo(987.97);
+  });
+
+  it('should correct for Unilever', () => {
+    const total = checkout(UNILIVER)([CLASSIC_AD,CLASSIC_AD,CLASSIC_AD,PREMIUM_AD]);
+    expect(total).toBeCloseTo(934.97);
+  });
+
+  it('should correct for Apple', () => {
+    const total = checkout(APPLE)([STANDOUT_AD,STANDOUT_AD,STANDOUT_AD,PREMIUM_AD]);
+    expect(total).toBeCloseTo(1294.96);
+  });
+
+  it('should correct for Nike', () => {
+    const total = checkout(NIKE)([PREMIUM_AD,PREMIUM_AD,PREMIUM_AD,PREMIUM_AD]);
+    expect(total).toBeCloseTo(1519.96);
+  });
 })
